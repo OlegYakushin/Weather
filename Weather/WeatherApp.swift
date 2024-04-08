c51c62c35d2c9ff2e5ae8f5040129eb31b57be4b
Oleg <yakushinoa@gmail.com>
2024-04-08 14:23:32 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Oleg Yakushin on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
